223ced936d7c177fc841d541d283db3dc735d93d
shbchk <kuchev@gmail.com>
2023-03-04 17:54:11 +0400

    various changes and additions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Классный мессенджер
+
+Макет тут: https://www.figma.com/file/IjLW6OLjj7h7qNgEiaM9KI/Chat-Yandex-Praktikum?node-id=0%3A1&t=1ZWh1uxqVEoGeaZ7-0
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Handlebars = require('handlebars');
+import ('normalize.css')
+import ('./components/auth/styles.less')
+
+const root = document.getElementById('root');
