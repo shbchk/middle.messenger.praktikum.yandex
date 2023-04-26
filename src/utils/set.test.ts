1ae341e29d94fc310b58d0b75febe70b0353967e
shbchk <dmitry@kuchev.com>
2023-04-26 11:33:30 +0400

    mocha added

---
+++ src/utils/set.test.ts
@@ -0,0 +1,13 @@
+// import { expect } from 'chai';
+import set from './set';
+
+describe('function set', () => {
+  it('should throw an Error if path not a string', () => {});
+
+  it.only('should return first param of it is not an object', () => {
+    set(null, '', '');
+    // expect(result).to.equal(null);
+  });
+
+  it('should throw an Error if path not a string', () => {});
+});
